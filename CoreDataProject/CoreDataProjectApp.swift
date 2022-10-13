61b6e1bd77996b18f73dbab7d8876bb21c4f0a9c
Carlossiii <cv5673@gmail.com>
2022-10-13 13:34:06 -0300

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Carlos Vinicius on 13/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
